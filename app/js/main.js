64b7e5ea239c34d1559525b9d21bd47ce7f585d9
Andrey-it03 <a_ivanov50@mail.ru>
2023-12-20 12:56:21 +0300

    added section tabs

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,15 @@ $(function(){
         prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="../images/icon/arrow-left.svg" alt=""></button>',
         nextArrow:'<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="../images/icon/arrow-right.svg" alt=""></button>'
     });
+
+
+    $('.search__tabs-item').on('click', function(e){
+        e.preventDefault(); //отключение ссылок
+        
+        $('.search__tabs-item').removeClass('search__tabs-item--active'); // удаление 2 класса
+        $('.search__content-item').removeClass('search__content-item--active');
+
+        $(this).addClass('search__tabs-item--active'); // добавление класса при клике
+        $($(this).attr('href')).addClass('search__content-item--active'); // добавление класса по id
+    })
 });
